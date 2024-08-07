c61150117fa25523fc728c9099e48f2117e2f4aa
Sahadev1431 <naisahadev@gmail.com>
2024-08-07 12:43:08 +0530

    complete 1- add two parameters inside String

--- StringCalculater.java
+++ StringCalculater.java
@@ -1,6 +1,9 @@
 public class StringCalculater {
     int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        return Integer.parseInt(numbers);
+        if (numbers.length() == 1) return Integer.parseInt(numbers);
+        String[] splittedArr = numbers.split(",");
+
+        return Integer.parseInt(splittedArr[0]) + Integer.parseInt(splittedArr[1]);
     }
 }
--- StringCalculaterTest.java
+++ StringCalculaterTest.java
@@ -4,15 +4,24 @@ import org.junit.Test;
 
 public class StringCalculaterTest {
 
+    // ""
     @Test
     public void emptyStringShouldReturnZero() {
         StringCalculater calculater = new StringCalculater();
         assertEquals(0, calculater.add(""));
     }
 
+    // "1"
     @Test
     public void oneArgumentShouldReturnNumberItSelf() {
         StringCalculater calculater = new StringCalculater();
-        assertEquals(5, calculater.add("5"));
+        assertEquals(1, calculater.add("1"));
+    }
+
+    // "1,2"
+    @Test
+    public void TwoArgsSum() {
+        StringCalculater calculater = new StringCalculater();
+        assertEquals(3, calculater.add("1,2"));
     }
 }
