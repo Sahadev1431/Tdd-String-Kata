8bb4a193ef819da05370c4f492125f55a6201f12
Sahadev1431 <naisahadev@gmail.com>
2024-08-08 21:41:46 +0530

    refactor the code for different delimeter

--- StringCalculater.java
+++ StringCalculater.java
@@ -1,24 +1,25 @@
 public class StringCalculater {
     int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        // if (numbers.length() == 1) return Integer.parseInt(numbers);
+
         String[] splittedArr = numbers.split(",|\n|;");
 
-        int addition = 0;
+        int ans = 0;
         if (splittedArr[0].startsWith("//")) {
-            for (int i = 1; i < splittedArr.length; i++) {
-
-                if (!splittedArr[i].isEmpty()) {
-                    addition += Integer.parseInt(splittedArr[i]);
-                }
-            }
+            ans = addAll(1, splittedArr);
         } else {
-            for (int i = 0; i < splittedArr.length; i++) {
+            ans = addAll(0, splittedArr);
+        }
+        return ans;
+    }
+
+    int addAll(int start, String[]splittedArr) {
+        int addition = 0;
+        for (int i = start; i < splittedArr.length; i++) {
+            if (!splittedArr[i].isEmpty()) {
                 addition += Integer.parseInt(splittedArr[i]);
             }
         }
-        
-
         return addition;
     }
 }
